6bb0a4852e94c2de32ee97d301ea45a9fad4910b
Gennady Zykov <zykovgennady@gmail.com>
2021-10-16 16:12:39 +0300

    Implemented rounding for activity "start at" and "end at".

--- GActivityDiary.Core/Helpers/TimeRounderHelper.cs
+++ GActivityDiary.Core/Helpers/TimeRounderHelper.cs
@@ -12,9 +12,55 @@ namespace GActivityDiary.Core.Helpers
     /// </summary>
     public class TimeRounderHelper
     {
+        /// <summary>
+        /// Rounds a start and end datetimes to the nearest hour values, 
+        /// and rounds midpoint values to the next hour.
+        /// Only hours and minutes are taken into calculate.
+        /// </summary>
+        /// <param name="activity"></param>
         public static void Round(Activity activity)
         {
-            throw new NotImplementedException();
+            if (activity == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (!activity.StartAt.HasValue && !activity.EndAt.HasValue)
+            {
+                return;
+            }
+            if (activity.StartAt.HasValue
+                && activity.EndAt.HasValue
+                && activity.StartAt.Value >= activity.EndAt.Value)
+            {
+                activity.EndAt = activity.StartAt?.AddMinutes(30);
+            }
+            if (activity.StartAt.HasValue)
+            {
+                activity.StartAt = Round(activity.StartAt.Value);
+            }
+            if (activity.EndAt.HasValue)
+            {
+                activity.EndAt = Round(activity.EndAt.Value);
+            }
+        }
+
+        /// <summary>
+        /// Rounds a <see cref="DateTime"/> value to the nearest hour value, 
+        /// and rounds midpoint values to the next hour.
+        /// Only hours and minutes are taken into calculate.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime Round(DateTime dateTime)
+        {
+            TimeSpan roundedTimeSpan = Round(dateTime.TimeOfDay);
+            return new DateTime(
+                dateTime.Year, 
+                dateTime.Month, 
+                dateTime.Day, 
+                roundedTimeSpan.Hours, 
+                roundedTimeSpan.Minutes, 
+                0);
         }
 
         /// <summary>
@@ -44,9 +90,51 @@ namespace GActivityDiary.Core.Helpers
             return new TimeSpan(hours, minutes, 0);
         }
 
+        /// <summary>
+        /// Returns the smallest hour-rounded value that is greater than or equal to the current values.
+        /// </summary>
+        /// <param name="activity"></param>
         public static void Ceiling(Activity activity)
         {
-            throw new NotImplementedException();
+            if (activity == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (!activity.StartAt.HasValue && !activity.EndAt.HasValue)
+            {
+                return;
+            }
+            if (activity.StartAt.HasValue
+                && activity.EndAt.HasValue
+                && activity.StartAt >= activity.EndAt)
+            {
+                activity.EndAt = activity.StartAt?.AddMinutes(30);
+            }
+            if (activity.StartAt.HasValue)
+            {
+                activity.StartAt = Ceiling(activity.StartAt.Value);
+            }
+            if (activity.EndAt.HasValue)
+            {
+                activity.EndAt = Ceiling(activity.EndAt.Value);
+            }
+        }
+
+        /// <summary>
+        /// Returns the smallest hour-rounded value that is greater than or equal to the current value.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime Ceiling(DateTime dateTime)
+        {
+            TimeSpan roundedTimeSpan = Ceiling(dateTime.TimeOfDay);
+            return new DateTime(
+                dateTime.Year,
+                dateTime.Month,
+                dateTime.Day,
+                roundedTimeSpan.Hours,
+                roundedTimeSpan.Minutes,
+                0);
         }
 
         /// <summary>
@@ -74,9 +162,52 @@ namespace GActivityDiary.Core.Helpers
             return new TimeSpan(hours, minutes, 0);
         }
 
+        /// <summary>
+        /// Returns the largest hour-rounded start and end datetimes 
+        /// that is less than or equal to the current values.
+        /// </summary>
+        /// <param name="activity"></param>
         public static void Floor(Activity activity)
         {
-            throw new NotImplementedException();
+            if (activity == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (!activity.StartAt.HasValue && !activity.EndAt.HasValue)
+            {
+                return;
+            }
+            if (activity.StartAt.HasValue
+                && activity.EndAt.HasValue
+                && activity.StartAt >= activity.EndAt)
+            {
+                activity.EndAt = activity.StartAt?.AddMinutes(30);
+            }
+            if (activity.StartAt.HasValue)
+            {
+                activity.StartAt = Floor(activity.StartAt.Value);
+            }
+            if (activity.EndAt.HasValue)
+            {
+                activity.EndAt = Floor(activity.EndAt.Value);
+            }
+        }
+
+        /// <summary>
+        /// Returns the largest hour-rounded value that is less than or equal to the current value.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime Floor(DateTime dateTime)
+        {
+            TimeSpan roundedTimeSpan = Floor(dateTime.TimeOfDay);
+            return new DateTime(
+                dateTime.Year,
+                dateTime.Month,
+                dateTime.Day,
+                roundedTimeSpan.Hours,
+                roundedTimeSpan.Minutes,
+                0);
         }
 
         /// <summary>
--- Tests/GActivityDiary.Core.Tests/DateTimeOperations/RoundingTimeTests.cs
+++ Tests/GActivityDiary.Core.Tests/DateTimeOperations/RoundingTimeTests.cs
@@ -1,4 +1,5 @@
 ﻿using GActivityDiary.Core.Helpers;
+using GActivityDiary.Core.Models;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -18,20 +19,109 @@ namespace GActivityDiary.Core.Tests.DateTimeOperations
             TimeSpan timeSpan3 = new(8, 30, 0);
             TimeSpan timeSpan4 = new(23, 30, 0);
 
+            // Round
             Assert.AreEqual(TimeRounderHelper.Round(timeSpan1), new TimeSpan(1, 0, 0));
             Assert.AreEqual(TimeRounderHelper.Round(timeSpan2), new TimeSpan(4, 0, 0));
             Assert.AreEqual(TimeRounderHelper.Round(timeSpan3), new TimeSpan(9, 0, 0));
             Assert.AreEqual(TimeRounderHelper.Round(timeSpan4), new TimeSpan(23, 59, 0));
 
+            // Floor
+            Assert.AreEqual(TimeRounderHelper.Floor(timeSpan1), new TimeSpan(1, 0, 0));
+            Assert.AreEqual(TimeRounderHelper.Floor(timeSpan2), new TimeSpan(4, 0, 0));
+            Assert.AreEqual(TimeRounderHelper.Floor(timeSpan3), new TimeSpan(8, 0, 0));
+            Assert.AreEqual(TimeRounderHelper.Floor(timeSpan4), new TimeSpan(23, 00, 0));
+
+            // Ceiling
             Assert.AreEqual(TimeRounderHelper.Ceiling(timeSpan1), new TimeSpan(1, 0, 0));
             Assert.AreEqual(TimeRounderHelper.Ceiling(timeSpan2), new TimeSpan(5, 0, 0));
             Assert.AreEqual(TimeRounderHelper.Ceiling(timeSpan3), new TimeSpan(9, 0, 0));
             Assert.AreEqual(TimeRounderHelper.Ceiling(timeSpan4), new TimeSpan(23, 59, 0));
 
-            Assert.AreEqual(TimeRounderHelper.Floor(timeSpan1), new TimeSpan(1, 0, 0));
-            Assert.AreEqual(TimeRounderHelper.Floor(timeSpan2), new TimeSpan(4, 0, 0));
-            Assert.AreEqual(TimeRounderHelper.Floor(timeSpan3), new TimeSpan(8, 0, 0));
-            Assert.AreEqual(TimeRounderHelper.Floor(timeSpan4), new TimeSpan(23, 00, 0));
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ActivityRoundingTest()
+        {
+            // Original datetimes
+            DateTime startAt1 = new(2021, 10, 16, 15, 44, 45);
+            DateTime endAt2 = new(2021, 10, 16, 16, 2, 1);
+            DateTime startAt3 = new(2021, 10, 16, 15, 0, 0);
+            DateTime endAt3 = new(2021, 10, 16, 14, 44, 0);
+
+            // Init activities
+            Activity activity1 = new()
+            {
+                Name = "Test activity 1",
+                CreatedAt = DateTime.Now,
+                StartAt = startAt1
+            };
+            Activity activity2 = new()
+            {
+                Name = "Test activity 2",
+                CreatedAt = DateTime.Now,
+                EndAt = endAt2
+            };
+            Activity activity3 = new()
+            {
+                Name = "Test activity 3",
+                CreatedAt = DateTime.Now,
+                StartAt = startAt3,
+                EndAt = endAt3
+            };
+            Assert.Greater(activity3.StartAt, activity3.EndAt);
+
+            // Round
+            TimeRounderHelper.Round(activity1);
+            Assert.IsNull(activity1.EndAt);
+            Assert.AreEqual(activity1.StartAt, new DateTime(2021, 10, 16, 16, 0, 0));
+
+            TimeRounderHelper.Round(activity2);
+            Assert.IsNull(activity2.StartAt);
+            Assert.AreEqual(activity2.EndAt, new DateTime(2021, 10, 16, 16, 0, 0));
+
+            TimeRounderHelper.Round(activity3);
+            Assert.IsNotNull(activity3.StartAt);
+            Assert.IsNotNull(activity3.EndAt);
+            Assert.Greater(activity3.EndAt, activity3.StartAt);
+
+            activity1.StartAt = startAt1;
+            activity2.EndAt = endAt2;
+            activity3.StartAt = startAt3;
+            activity3.EndAt = endAt3;
+
+            // Floor
+            TimeRounderHelper.Floor(activity1);
+            Assert.IsNull(activity1.EndAt);
+            Assert.AreEqual(activity1.StartAt, new DateTime(2021, 10, 16, 15, 0, 0));
+
+            TimeRounderHelper.Floor(activity2);
+            Assert.IsNull(activity2.StartAt);
+            Assert.AreEqual(activity2.EndAt, new DateTime(2021, 10, 16, 16, 0, 0));
+
+            TimeRounderHelper.Floor(activity3);
+            Assert.IsNotNull(activity3.StartAt);
+            Assert.IsNotNull(activity3.EndAt);
+            Assert.AreEqual(activity3.EndAt, activity3.StartAt);
+
+            activity1.StartAt = startAt1;
+            activity2.EndAt = endAt2;
+            activity3.StartAt = startAt3;
+            activity3.EndAt = endAt3;
+
+            // Ceiling
+            TimeRounderHelper.Ceiling(activity1);
+            Assert.IsNull(activity1.EndAt);
+            Assert.AreEqual(activity1.StartAt, new DateTime(2021, 10, 16, 16, 0, 0));
+
+            TimeRounderHelper.Ceiling(activity2);
+            Assert.IsNull(activity2.StartAt);
+            Assert.AreEqual(activity2.EndAt, new DateTime(2021, 10, 16, 17, 0, 0));
+
+            TimeRounderHelper.Ceiling(activity3);
+            Assert.IsNotNull(activity3.StartAt);
+            Assert.IsNotNull(activity3.EndAt);
+            Assert.Greater(activity3.EndAt, activity3.StartAt);
 
             Assert.Pass();
         }
